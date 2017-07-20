ca1d755199a6cff2ef312940fe93c7c19e3d5d7a
Wang <Albert.wang@covidien.com>
2017-07-20 15:38:02 +0800

    the first game using openGL.

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"math/rand"
 	"runtime"
 
 	"log"
+
+	"fmt"
+	"strings"
+
+	"time"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
@@ -12,13 +18,118 @@ import (
 const (
 	width  = 512
 	height = 512
+
+	rows    = 200
+	columns = 200
+
+	threshold = 0.2
+	fps       = 60
 )
 
 var (
 	triangle = []float32{
-		0, 0.5, 0, -0.5, -0.5, 0, 0.5, -0.5, 0,
+		0, 0.5, 0,
+		-0.5, -0.5, 0,
+		0.5, -0.5, 0,
 	}
+	square = []float32{
+		-0.5, 0.5, 0,
+		-0.5, -0.5, 0,
+		0.5, -0.5, 0,
+		-0.5, 0.5, 0,
+		0.5, 0.5, 0,
+		0.5, -0.5, 0,
+	}
+	vertexShaderSource = `
+    #version 410
+    in vec3 vp;
+    void main() {
+        gl_Position = vec4(vp, 1.0);
+    }
+` + "\x00"
+
+	fragmentShaderSource = `
+    #version 410
+    out vec4 frag_colour;
+    void main() {
+        frag_colour = vec4(1, 1, 1, 1);
+    }
+` + "\x00"
 )
+
+type cell struct {
+	drawable uint32
+	x        int
+	y        int
+
+	alive     bool
+	aliveNext bool
+}
+
+func (c *cell) draw() {
+	if !c.alive {
+		return
+	}
+	gl.BindVertexArray(c.drawable)
+	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(square)/3))
+}
+
+func (c *cell) checkstate(cells [][]*cell) {
+	c.alive = c.aliveNext
+	c.aliveNext = c.alive
+
+	liveCount := c.liveNeighours(cells)
+	if c.alive {
+		if liveCount < 2 {
+			c.aliveNext = false
+		}
+
+		if liveCount == 2 || liveCount == 3 {
+			c.aliveNext = true
+		}
+
+		if liveCount > 3 {
+			c.aliveNext = false
+		}
+	} else {
+		if liveCount == 3 {
+			c.aliveNext = true
+		}
+	}
+
+}
+
+func (c *cell) liveNeighours(cells [][]*cell) int {
+	var liveCount int
+	add := func(x, y int) {
+		if x == len(cells) {
+			x = 0
+		} else if x == -1 {
+			x = len(cells) - 1
+		}
+		if y == len(cells[x]) {
+			y = 0
+		} else if y == -1 {
+			y = len(cells[x]) - 1
+		}
+
+		if cells[x][y].alive {
+			liveCount++
+		}
+	}
+
+	add(c.x-1, c.y)
+	add(c.x+1, c.y)
+	add(c.x, c.y-1)
+	add(c.x, c.y+1)
+	add(c.x-1, c.y+1)
+	add(c.x+1, c.y+1)
+	add(c.x-1, c.y-1)
+	add(c.x+1, c.y-1)
+
+	return liveCount
+
+}
 
 func initEnv() {
 	runtime.LockOSThread()
@@ -52,7 +163,18 @@ func initOpenGL() uint32 {
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	log.Println("OpenGL version:", version)
 
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
 	gl.LinkProgram(prog)
 	return prog
 }
@@ -63,6 +185,20 @@ func draw(vao uint32, window *glfw.Window, program uint32) {
 
 	gl.BindVertexArray(vao)
 	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle)/3))
+
+	glfw.PollEvents()
+	window.SwapBuffers()
+}
+
+func drawSqure(cells [][]*cell, window *glfw.Window, program uint32) {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(program)
+
+	for x := range cells {
+		for _, c := range cells[x] {
+			c.draw()
+		}
+	}
 
 	glfw.PollEvents()
 	window.SwapBuffers()
@@ -81,8 +217,73 @@ func makeVao(points []float32) uint32 {
 	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
 	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
 
-	log.Println("vao:", vao)
+	//	log.Println("vao:", vao)
 	return vao
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v:%v", source, log)
+	}
+	return shader, nil
+}
+
+func newCell(x, y int) *cell {
+	points := make([]float32, len(square), len(square))
+	copy(points, square)
+
+	for i := 0; i < len(points); i++ {
+		var position float32
+		var size float32
+		switch i % 3 {
+		case 0:
+			size = 1.0 / float32(columns)
+			position = float32(x) * size
+		case 1:
+			size = 1.0 / float32(rows)
+			position = float32(y) * size
+		default:
+			continue
+		}
+		if points[i] < 0 {
+			points[i] = (position * 2) - 1
+		} else {
+			points[i] = ((position + size) * 2) - 1
+		}
+	}
+
+	return &cell{
+		drawable: makeVao(points),
+		x:        x,
+		y:        y,
+	}
+}
+
+func makeCells() [][]*cell {
+	rand.Seed(time.Now().UnixNano())
+	cells := make([][]*cell, rows, columns)
+	for x := 0; x < rows; x++ {
+		for y := 0; y < columns; y++ {
+			c := newCell(x, y)
+			c.alive = rand.Float64() < threshold
+			c.aliveNext = c.alive
+			cells[x] = append(cells[x], c)
+		}
+	}
+	return cells
 }
 
 func main() {
@@ -92,10 +293,22 @@ func main() {
 	program := initOpenGL()
 	log.Println("program:", program)
 
-	vao := makeVao(triangle)
+	//vao := makeVao(triangle)
+	cells := makeCells()
 	for !window.ShouldClose() {
 
-		draw(vao, window, program)
+		t := time.Now()
+
+		for x := range cells {
+			for _, c := range cells[x] {
+				c.checkstate(cells)
+			}
+		}
+
+		//	draw(vao, window, program)
+		drawSqure(cells, window, program)
+
+		time.Sleep(time.Second/time.Duration(fps) - time.Since(t))
 
 	}
 
